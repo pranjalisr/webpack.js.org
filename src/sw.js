41d47636eb066345dbc3cc0cc0267ff36ff09414
Pranjali <pranjalisr25@gmail.com>
2026-02-15 21:22:27 +0530

    fixed prettier

--- src/sw.js
+++ src/sw.js
@@ -26,15 +26,15 @@ const otherManifest = [
   },
 ];
 const EXCLUDED_PRECACHE_URLS = new Set([
-   new URL("/icon_512x512.png", self.location).href,
+  new URL("/icon_512x512.png", self.location).href,
 ]);
 const manifestURLs = [...manifest, ...otherManifest]
-.map((entry) => new URL(entry.url, self.location).href)
-.filter((href) => !EXCLUDED_PRECACHE_URLS.has(href));
+  .map((entry) => new URL(entry.url, self.location).href)
+  .filter((href) => !EXCLUDED_PRECACHE_URLS.has(href));
 
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => cache.addAll(manifestURLs))
+    caches.open(cacheName).then((cache) => cache.addAll(manifestURLs)),
   );
 });
 self.addEventListener("activate", (event) => {
@@ -45,10 +45,12 @@ self.addEventListener("activate", (event) => {
       const keys = await cache.keys();
       await Promise.all(
         keys.map((request) =>
-          manifestURLs.includes(request.url) ? Promise.resolve() : cache.delete(request),
-      ),
-    );
-   }) (),
+          manifestURLs.includes(request.url)
+            ? Promise.resolve()
+            : cache.delete(request),
+        ),
+      );
+    })(),
   );
 });
 registerRoute(
