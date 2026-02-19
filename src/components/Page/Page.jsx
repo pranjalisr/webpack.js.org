6c2cc285666c93661ae94b7a8adc910ed089d0e9
Pranjali <pranjalisr25@gmail.com>
2026-02-19 11:50:48 +0530

    added main

--- src/components/Page/Page.jsx
+++ src/components/Page/Page.jsx
@@ -100,6 +100,7 @@ export default function Page(props) {
   }
   return (
     <section className="page">
+      <main id="main-content">
       <Markdown>
         <h1>{title}</h1>
 
@@ -143,6 +144,7 @@ export default function Page(props) {
           </div>
         )}
       </Markdown>
+        </main>
     </section>
   );
 }
