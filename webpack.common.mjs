bb68cf3385aec0ace7eae598b517736db9af2935
Pranjali <pranjalisr25@gmail.com>
2026-02-21 19:44:12 +0530

    fix: adjust hastscript import

--- webpack.common.mjs
+++ webpack.common.mjs
@@ -1,6 +1,8 @@
 import { createRequire } from "node:module";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { h } from "hastscript";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import autolink from "remark-autolink-headings";
 import remarkEmoji from "remark-emoji";
@@ -19,19 +21,6 @@ const __dirname = path.dirname(__filename);
 
 const require = createRequire(import.meta.url);
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const hastscriptMod = require("hastscript");
-
-const hastscriptFn =
-  (typeof hastscriptMod === "function" && hastscriptMod) ||
-  (typeof hastscriptMod?.h === "function" && hastscriptMod.h) ||
-  (typeof hastscriptMod?.default === "function" && hastscriptMod.default) ||
-  (typeof hastscriptMod.default?.h === "function" && hastscriptMod.default.h) ||
-  null;
-
-if (!hastscriptFn) {
-  throw new Error("Unable to resolve hastscript export");
-}
 const mdPlugins = [
   gfm,
   slug,
@@ -53,7 +42,7 @@ const mdPlugins = [
     {
       behavior: "append",
       content() {
-        return [hastscriptFn("span.header-link")];
+        return [h("span.header-link")];
       },
     },
   ],
