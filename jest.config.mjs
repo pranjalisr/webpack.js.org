06b6a787ba591fad7e3c4778843ac4d508a0eb06
Chirag Patil <160267821+chirag-patil625@users.noreply.github.com>
2026-02-25 20:01:11 +0530

    feat(offline): add OfflineBanner component and tests (#7868)

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,39 +1,28 @@
 export default {
   verbose: true,
-  projects: [
-    {
-      displayName: "utils",
-      testEnvironment: "node",
-      transform: {},
-      moduleFileExtensions: [
-        "js",
-        "mjs",
-        "cjs",
-        "jsx",
-        "ts",
-        "tsx",
-        "json",
-        "node",
-      ],
-      testMatch: [
-        "**/__tests__/**/*.[jt]s?(x)",
-        "**/?(*.)+(spec|test).[tj]s?(x)",
-        "**/*.test.mjs",
-      ],
-      testPathIgnorePatterns: ["/node_modules/", "\\.test\\.jsx$"],
-    },
-    {
-      displayName: "components",
-      testEnvironment: "jsdom",
-      transform: {
-        "^.+\\.jsx?$": "babel-jest",
-      },
-      moduleNameMapper: {
-        "\\.(scss|css)$": "<rootDir>/src/components/__mocks__/styleMock.js",
-        "\\.svg$": "<rootDir>/src/components/__mocks__/svgMock.js",
-      },
-      moduleFileExtensions: ["js", "jsx", "json", "node"],
-      testMatch: ["**/src/components/**/*.test.jsx"],
-    },
+  testEnvironment: "node",
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+  },
+  moduleNameMapper: {
+    "\\.(scss|css)$": "<rootDir>/src/components/__mocks__/styleMock.js",
+    "\\.svg$": "<rootDir>/src/components/__mocks__/svgMock.js",
+  },
+  moduleFileExtensions: [
+    "js",
+    "mjs",
+    "cjs",
+    "jsx",
+    "ts",
+    "tsx",
+    "json",
+    "node",
   ],
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)",
+    "**/*.test.mjs",
+    "**/src/components/**/*.test.jsx",
+  ],
+  testPathIgnorePatterns: ["/node_modules/"],
 };
